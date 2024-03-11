71c87591e33721a61d53d22e3ba03c2dc7095f7e
Fabiolll <fabio95.df@gmail.com>
2024-03-11 10:55:38 -0300

    criacao do projeto

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace PedidoEstruturado;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double porcentagem, totalD = 0;
+        Console.WriteLine("Nome: ");
+        string nomeP = Convert.ToString(Console.ReadLine());
+        Console.WriteLine("Preço: ");
+        double preco = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Quantidade: ");
+        double quantidade = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("-----------------REVISAO PEDIDO-----------------------------");
+        if (quantidade >= 11 && quantidade <= 20)
+        {
+            double total = preco * quantidade;
+            porcentagem = (total / 100) * 10;
+            totalD = total - porcentagem;
+            Console.WriteLine($"Nome: {nomeP}\nPreço: {preco}\nValor total sem desconto: {total}\nValor total com desconto: {totalD}");
+        }
+
+        else if (quantidade >= 21 && quantidade <= 50)
+        {
+            double total = preco * quantidade;
+            porcentagem = (total / 100) * 20;
+            totalD = total - porcentagem;
+            Console.WriteLine($"Nome: {nomeP}\nPreço: {preco}\nValor total sem desconto: {total}\nValor total com desconto: {totalD}");
+        }
+        else if (quantidade > 50)
+        {
+            double total = preco * quantidade;
+            porcentagem = (total / 100) * 25;
+            totalD = total - porcentagem;
+            Console.WriteLine($"Nome: {nomeP}\nPreço: {preco}\nValor total sem desconto: {total}\nValor total com desconto: {totalD}");
+        }
+        else
+        {
+            double total = preco * quantidade;
+            Console.WriteLine($"Nome: {nomeP}\nPreço: {preco}\nValor total sem desconto: {total}\nValor total com desconto: {total}");
+        }
+
+    }
+}
